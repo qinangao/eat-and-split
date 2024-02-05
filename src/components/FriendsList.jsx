b07b77000f32f26947c94199cbd9d82b2bb46b54
Qinan Gao <qinanagao@gmail.com>
2024-02-05 22:15:28 +0000

    move sidebar div to app

--- src/components/FriendsList.jsx
+++ src/components/FriendsList.jsx
@@ -2,12 +2,10 @@ import initialFriends from "./data";
 import Friend from "./Friend";
 export default function FriendsList() {
   return (
-    <div className="sidebar">
-      <ul>
-        {initialFriends.map((friend) => (
-          <Friend friend={friend} key={friend.id} />
-        ))}
-      </ul>
-    </div>
+    <ul>
+      {initialFriends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
   );
 }
