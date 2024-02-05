793399c40728373ad05950fa24853b3e5eec572b
Qinan Gao <qinanagao@gmail.com>
2024-02-05 22:14:37 +0000

    friend form

---
+++ src/components/FormAddFriend.jsx
@@ -0,0 +1,13 @@
+import Button from "./Button";
+
+export default function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👭Friend name</label>
+      <input type="text" />
+      <label>🔅Image URL</label>
+      <input type="text" />
+      <Button>Add</Button>
+    </form>
+  );
+}
